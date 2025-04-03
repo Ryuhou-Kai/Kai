855603a1f84d308f868a1db68657b82c24350415
no8f <snipez5002@gmail.com>
2025-04-03 21:25:04 +0200

    - added dynamic bot tokens - removed test function

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
+<p align="center">
+ <a href="#"><img src="https://img.shields.io/badge/c++-%2300427e.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white"/></a>
+ <a href="#"><img src="https://img.shields.io/badge/CMake-%23eb2633.svg?style=for-the-badge&logo=cmake&logoColor=white"/></a>
+</p>
+
 # Kai
 
 A small but nice Discord bot, for our discord server, does mostly small work and personalized commands that the big bots don't support, and a bit of quality of life shit
 
 ## Features
 
-- None. I created this 5 minutes ago, what do you expect
+- Answers with a cat picture upon "!cat" /ᐠ ˵> ⩊ <˵マ
+
+# Build/Contributing
+
+## Dependencies
+
+- cmake >=3.3
+- A modern c++ compiler (one that can handle c++ 22)
+- Git
+
+Open the root CMakeLists.txt in a coder editor/IDE that can:
+    
+ 1. Handle cmake presets
+ 2. Supports clang-format 
+   
+For development, select the debug profile and start the cmake, followed by a build.
+
+>[!WARNING]
+>The build will most likely fail, due to two missing includes in the d++ lib
+>There is a patch for this in the “patches” folder, just go to the build/_deps/dpp-src directory and `git apply patch <path/to/patch>`.
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,28 @@
 #include <dpp/dpp.h>
 #include <string>
 
+#include "config.h"
+//PLS READ!!!!!!!!!!!!!!!!
+#if DEBUG_VALUE // This is the Token for testing replace with your own bot Token!
 const std::string BOT_TOKEN = "MTM1NTUzNzM5MTU5ODgyOTU3Ng.G2LXKA.A0jNHaGhZ1yU0zvTxI32kMhvMO-gMO4J2rZypg";
+#else// This is the Token for the Bot on the discord server
+const std::string BOT_TOKEN = "MTM1NjY1NzIwMTkwNDYxOTc4Mg.GHSer4.suQ1pyJg5oCRjXqGy7oABmZRBYCzJLxGbGj0uY";
+#endif
 
 int main() {
 	dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
 
 	bot.on_log(dpp::utility::cout_logger());
-	/*Theeventisfiredwhensomeoneissuesyourcommands*/
-	bot.on_slashcommand([&bot](const dpp::slashcommand_t &event) {
-		/*Checkwhichcommandtheyran*/
-		if (event.command.get_command_name() == "embed") {
-			/*Createanembed*/
-			dpp::embed embed = dpp::embed()
-									   .set_color(dpp::colors::sti_blue)
-									   .set_title("Somename")
-									   .set_url("https://dpp.dev/")
-									   .set_author("Somename", "https://dpp.dev/", "https://dpp.dev/DPP-Logo.png")
-									   .set_description("Somedescriptionhere")
-									   .set_thumbnail("https://dpp.dev/DPP-Logo.png")
-									   .add_field("Regularfieldtitle", "Somevaluehere")
-									   .add_field("Inlinefieldtitle", "Somevaluehere", true)
-									   .add_field("Inlinefieldtitle", "Somevaluehere", true)
-									   .set_image("https://dpp.dev/DPP-Logo.png")
-									   .set_footer(dpp::embed_footer()
-														   .set_text("Somefootertexthere")
-														   .set_icon("https://dpp.dev/DPP-Logo.png"))
-									   .set_timestamp(time(0));
-
-			/*Createamessagewiththecontentasournewembed.*/
-			dpp::message msg(event.command.channel_id, embed);
-
-			/*Replytotheuserwiththemessage,containingourembed.*/
-			event.reply(msg);
-		}
-	});
 
 	bot.on_message_create([&bot](const dpp::message_create_t &event) {
-		/*Seeifthemessagecontainsthephrasewewanttocheckfor.
-		 *Ifthere'satleastasinglematch,wereplyandsayit'snotallowed.
-		 */
 		if (event.msg.content.find("!cat") != std::string::npos) {
 			bot.request("https://cataas.com/cat", dpp::m_get,
 						[event](const dpp::http_request_completion_t &httpRequestCompletion) {
-							/*Createamessage*/
-							dpp::message msg("Here'syourcat: /ᐠ-˕-マⳊ");
+							dpp::message msg("Here's your cat: /ᐠ-˕-マⳊ");
 
-							/*Attachtheimagetothemessage,onlyonsuccess(Code200).*/
 							if (httpRequestCompletion.status == 200)
 								msg.add_file("cat.png", httpRequestCompletion.body);
 
-							/*Sendthemessage,withourattachment.*/
 							event.reply(msg, true);
 						});
 		}
@@ -58,7 +30,6 @@ int main() {
 
 	bot.on_ready([&bot](const dpp::ready_t &event) {
 		if (dpp::run_once<struct register_bot_commands>()) {
-			/*Createandregisteracommandwhenthebotisready*/
 			bot.global_command_create(dpp::slashcommand("embed", "Sendatestembed!", bot.me.id));
 		}
 	});
